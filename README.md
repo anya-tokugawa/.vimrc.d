7fb94b8cf8e9caf1482ce7e65a7eb70819927bdf
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-07-12 21:56:17 +0900

    update Readme and fix somethings

---
+++ README.md
@@ -0,0 +1,22 @@
+# VIM RunCommand Configration
+
+![Image](image.png)
+
+## Bundling
+
+- Make `.vim`
+- Install Bundle Plugin
+
+
+## Installration
+
+```sh
+cd ~/
+git clone git@github.com:Eric-lightning/.vimrc.d
+cd .vimrc.d
+chmod u+x setup.sh
+./setup.sh # Setup and Install Bundle/Colorscheme
+```
+
+
+
--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
-#!/bin/bash -eux
+#!/bin/bash -ux
 ln -s $HOME/.vimrc.d/vimrc.txt $HOME/.vimrc
 $HOME/.vimrc.d/auto-insert.sh
 
 : "Bundle Settings"
 mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone github:VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+: "colorscheme Settings"
+mkdir -p $HOME/.vim/colors
+cd $HOME/.vim/colors
+curl -OL --progress https://raw.githubusercontent.com/Eric-lightning/vim-hackerman-syntax/master/colors/hackerman.vim
+cd $HOME
 vim +PluginInstall +qall
-
-
-
-echo "done!"
+: "done!"
