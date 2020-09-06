e5f26b8adf585bf48258acd12cf6bb60bf31cc43
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-09-06 09:29:56 +0900

    change colorscheme to molokai

--- setup.sh
+++ setup.sh
@@ -6,10 +6,14 @@ $HOME/.vimrc.d/mktools/auto-insert.sh
 mkdir -p ~/.vim/bundle
 git clone github:VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
-: "colorscheme Settings"
+: "collect colorscheme Settings"
+
 mkdir -p $HOME/.vim/colors
 cd $HOME/.vim/colors
 curl -OL --progress https://raw.githubusercontent.com/Eric-lightning/vim-hackerman-syntax/master/colors/hackerman.vim
+git clone https://github.com/tomasr/molokai.git molokai
+ln -s  molokai/colors/molokai.vim ./
+
 cd $HOME
 vim +PluginInstall +qall
 : "done!"
