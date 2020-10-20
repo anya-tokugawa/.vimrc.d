ea4b8570bcf826101fef2df57a120396d6a4f45a
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-10-20 14:32:09 +0900

    code-fmtを追加
    また、auto-insert.shを修正
    after-bundleconfを追加

--- mktools/auto-insert.sh
+++ mktools/auto-insert.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -eux
 
+
+mkdir -p ~/.vimrc.d/backups
+
 #*.conf
 if [[ -e ~/.vimrc.d/config ]];
 then
-	mv ~/.vimrc.d/config ~/.vimrc.d/config.$(date +'%Y%m%d_%H%M%S')
+	mv ~/.vimrc.d/config ~/.vimrc.d/backups/config.$(date +'%Y%m%d_%H%M%S')
 fi
 
 for i in $(ls -1 ~/.vimrc.d/*.conf)
@@ -15,11 +18,24 @@ done
 
 if [[ -e ~/.vimrc.d/bundle-include.list ]];
 then
-	mv ~/.vimrc.d/bundle-include.list ~/.vimrc.d/config.$(date +'%Y%m%d_%H%M%S')
+	mv ~/.vimrc.d/bundle-include.list ~/.vimrc.d/backups/bundle-include.$(date +'%Y%m%d_%H%M%S')
 fi
 
 
 for i in $(ls -1 ~/.vimrc.d/*.bundleconf)
 do
 echo "source $i" >> ~/.vimrc.d/bundle-include.list
+done
+
+# after-bundleconf
+
+if [[ -e ~/.vimrc.d/bundle-after.list ]];
+then
+	mv ~/.vimrc.d/bundle-after.list ~/.vimrc.d/backups/bundle-after.$(date +'%Y%m%d_%H%M%S')
+fi
+
+
+for i in $(ls -1 ~/.vimrc.d/*.after-bundleconf)
+do
+echo "source $i" >> ~/.vimrc.d/bundle-after.list
 done
