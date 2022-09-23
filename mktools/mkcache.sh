6b136545d6c12384c023667f14ee1054abe293ea
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2022-09-23 20:01:52 +0900

    Chnage Files: concate core configuitaion

--- mktools/mkcache.sh
+++ mktools/mkcache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 find conf.d -type f | xargs cat | grep -E '^Plugin' > ~/.vimrc.d/cache/plugins.conf
 find conf.d -type f | xargs cat | grep -vE '^Plugin' > ~/.vimrc.d/cache/noplugins.conf
+find auth.d -type f | xargs cat > ~/.vimrc.d/cache/auth.conf
--- setup.sh
+++ setup.sh
@@ -1,40 +1,24 @@
-#!/bin/bash -ux
-ln -s $HOME/.vimrc.d/vimrc.txt $HOME/.vimrc
-$HOME/.vimrc.d/mktools/auto-insert.sh
-
-mkdir $HOME/.vimrc.d/fmt/
+#!/bin/bash -u
 #gfmt_java_url="https://github.com/google/google-java-format/releases/download/v1.11.0/google-java-format-1.11.0-all-deps.jar"
 #gfmt_java_url="https://github.com/google/google-java-format/releases/download/google-java-format-1.8/google-java-format-1.8-all-deps.jar"
 gfmt_java_url="https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
 
-echo "google-java-format: version - v${gfmt_java_url}"
-[[ ! -e $HOME/.vimrc.d/fmt/google-java-fmt-all-deps.jar ]] && wget "$gfmt_java_url" -O $HOME/.vimrc.d/fmt/google-java-fmt-all-deps.jar
-
-: "Bundle Settings"
-mkdir -p ~/.vim/bundle
-[[ ! -e  $HOME/.vim/bundle/Vundle.vim ]] && git clone github:VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-[[ ! -e  $HOME/.vim/pack/vimspector ]] && git clone github:puremourning/vimspector $HOME/.vim/pack/vimspector
-
-: "collect colorscheme Settings"
-
-mkdir -p $HOME/.vim/colors
-cd $HOME/.vim/colors
-[[ ! -e hackerman.vim ]] && curl -OL --progress https://raw.githubusercontent.com/Eric-lightning/vim-hackerman-syntax/master/colors/hackerman.vim
-[[ ! -e molokai.vim ]] && git clone https://github.com/tomasr/molokai.git molokai && \
-ln -s  molokai/colors/molokai.vim ./
-
-cd $HOME
+mkdir -p "$HOME/.vimrc.d/etc/fmt" "$HOME/.vim/bundle" "$HOME/.vim/colors"
+[[ ! -e "$HOME/.vimrc" ]] && ln -s "$HOME/.vimrc.d/vimrc.txt" "$HOME/.vimrc" || echo "WARNING: .vimrc exist."
+[[ ! -e "$HOME/.vimrc.d/etc/fmt/google-java-fmt-all-deps.jar" ]] && wget "$gfmt_java_url" -O "$HOME/.vimrc.d/etc/fmt/google-java-fmt-all-deps.jar"
+[[ ! -e "$HOME/.vim/bundle/Vundle.vim" ]] && git clone https://github.com/VundleVim/Vundle.vim "$HOME/.vim/bundle/Vundle.vim"
+[[ ! -e "$HOME/.vim/pack/vimspector" ]] && git clone https://github.com/puremourning/vimspector "$HOME/.vim/pack/vimspector"
+[[ ! -e "$HOME/.vim/colors/molokai.vim" ]] && ln -s "$HOME/.vimrc.d/etc/colors/molokai.vim" "$HOME/.vim/colors"
 
 source /etc/os-release
+case "$NAME" in
+  "Ubuntu")
+    if ! dpkg -s exuberant-ctags > /dev/null 2>&1; then
+      sudo apt install exuberant-ctags
+    fi
+    ;;
+esac
 
-if [ $NAME = "Ubuntu" ]
-then
-  if ! (LC_ALL=C apt list exuberant-ctags | grep installed);then
-    sudo apt install exuberant-ctags
-  fi
-fi
-
-
-
+mktools/mkcache.sh
 vim +PluginInstall +qall
-: "done!"
+echo "Done."
